f0f33b80a92c56e40d0eb9cba8bd635e46317498
Gaurank Sharma <95328684+gaurank-sharma@users.noreply.github.com>
2026-02-26 20:35:53 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "@/index.css";
+// import App from "@/App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@/index.css";
@@ -5,7 +18,5 @@ import App from "@/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 );
